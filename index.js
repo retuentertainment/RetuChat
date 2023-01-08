05a5369ac53c7c7a18a53d98e1d867875752ca14
retuentertainment <122214332+retuentertainment@users.noreply.github.com>
2023-01-08 18:10:05 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -215,6 +215,19 @@ const firebaseConfig = {
         // then return/don't send the message. The user is somehow hacking
         // to send messages. Or they just deleted the
         // localstorage themselves. But hacking sounds cooler!!
+        // Get the input field
+var input = document.getElementById("myInput");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
         if(parent.get_name() == null && message == null){
           return
         }
